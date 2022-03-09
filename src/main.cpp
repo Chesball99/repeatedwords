40aafcbe1cd86b1e4bc49bca2faf4f2303d6d732
Alejandro Molina <amolinaegames@gmail.com>
2022-03-08 20:10:03 -0600

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "headers/std_lib_facilities.h"
+using namespace std;
+
+int main(){
+	string previous = " ";
+	string current;
+	while (cin >> current)
+	{
+		if (previous == current)
+			cout << "repeated word: " << current << '\n';
+		previous = current;
+	}	
+}
